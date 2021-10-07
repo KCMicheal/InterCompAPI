b59b0c0e1f0077b9eb49f41bb1a3d68e4a51f863
KCMicheal <kcmicheal@users.noreply.github.com>
2021-10-07 15:45:27 +0100

    Add project files.

---
+++ CompanyEmployees/Controllers/CompanyController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+
+        public CompanyController(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            var companies = _repository.Company.GetAllCompanies(trackChanges: false);
+
+            var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+
+            //throw new Exception("Exception");
+            return Ok(companiesDto);
+            
+        }
+
+        [HttpGet("{id}", Name = "CompanyById")]
+        public IActionResult GetCompany(Guid id)
+        {
+            var company = _repository.Company.GetCompany(id, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company with id: {id} dosen't exist in the database.");
+                return NotFound();
+            }
+            else
+            {
+                var companyDto = _mapper.Map<CompanyDto>(company);
+                return Ok(companyDto);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompany([FromBody]CompanyForCreationDto company)
+        {
+            if(company == null)
+            {
+                _logger.LogError("CompanyForCreationDto object sent from client is null.");
+                return BadRequest("CompanyForCreationDto is null.");
+            }
+            var companyEntity = _mapper.Map<Company>(company);
+
+            _repository.Company.CreateCompany(companyEntity);
+            _repository.Save();
+
+            var companyToReturn = _mapper.Map<CompanyDto>(companyEntity);
+            return CreatedAtRoute("CompanyById", new { id = companyToReturn.Id }, companyToReturn);
+        }
+    }
+}
---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,57 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        private readonly IRepositoryManager _respository;
+        /*private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };*/
+
+        //private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILoggerManager logger, IRepositoryManager repository)
+        {
+            _logger = logger;
+            _respository = repository;
+        }
+       
+        /*[HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }*/
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _logger.LogInfo("Here is info message from our values controller.");
+            _logger.LogDebug("Here is a debug message from our vvalues controller");
+            _logger.LogWarn("Here is a warn message from our values controller.");
+            _logger.LogError("Here is an error message from our values controller.");
+
+           /* _respository.Company.AnyMethodFromCompanyRepository();
+            _respository.Employee.AnyMethodFromEmployeeRepository();*/
+
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
