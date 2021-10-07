66f351767d2cba069077579192fd802eddaaf9bb
KCMicheal <kcmicheal@users.noreply.github.com>
2021-10-07 16:36:33 +0100

    child resources done

--- Entities/DataTransferObjects/EmployeeForCreationDto.cs
+++ Entities/DataTransferObjects/EmployeeForCreationDto.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Entities.DataTransferObjects
 {
-    class EmployeeForCreationDto
+    public class EmployeeForCreationDto
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
     }
 }
